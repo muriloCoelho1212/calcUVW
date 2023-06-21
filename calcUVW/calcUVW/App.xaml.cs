94081be266860d6ae3e620b4b991eba79a9b4dff
UlissesGimenez0 <134155000+UlissesGimenez0@users.noreply.github.com>
2023-06-20 21:12:48 -0300

    Sugestão de código (prof Alex)

--- calcUVW/calcUVW/App.xaml.cs
+++ calcUVW/calcUVW/App.xaml.cs
@@ -19,6 +19,26 @@ namespace calcUVW
         {
             base.OnStart();
 
+            /*
+             * Usando como "preferência" as cores para cada parte do layout
+             * Ao invés de usar como recurso do aplicativo, use o método Get
+             * No método Get, é buscado o nome da variável, junto com o valor padrão (pré-definido)
+             * Se a variável ainda não existe, vai usar o valor padrão
+             * Considere usar tema claro ou tema escuro como um padrão
+             * Depois o usuário pode escolher como quiser
+             */
+            string ButtonBackgroundColor = Preferences.Get("ButtonBackgroundColor", "#58B8F9");
+
+            /*
+             * Salvando como "preferência" as cores para cada parte do layout
+             * Ao invés de usar como recurso do aplicativo, use o método Set
+             * No método Set, é informado o nome da variável e seu valor
+             * Quando for usado o método Get, retornará o último valor salvo
+             * O método Set será usado quando o usuário trocar de tema
+             * AppShell.themeChanged_Clicked (linha 27)
+             */
+            Preferences.Set("ButtonBackgroundColor", "#0E76BC");
+            
             if ((int)Current.Properties["tema"] == 2)
             {
                 Current.UserAppTheme = OSAppTheme.Dark;
